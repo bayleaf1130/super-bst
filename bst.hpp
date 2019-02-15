a21b2424d8a15bc5364682986077f1b81b2a9c9a
bayleaf <bailey.kocin@gmail.com>
2019-02-15 14:23:11 -0500

    Update bst.hpp

--- bst.hpp
+++ bst.hpp
@@ -22,7 +22,7 @@
 namespace ads
 {
 	
-	/* Immutable binary search tree */
+	/*  binary search tree */
 	template<typename T>
 	class BinarySearchTree
 	{
@@ -100,12 +100,6 @@ namespace ads
 				invert(this->root);
 			}
 			
-	
-
-
-
-
-
 
 		private:
 			BSTNode* root = nullptr;
@@ -222,57 +216,21 @@ namespace ads
         
 				if(!root) return;
         
-    			ptr.push(root);
-          while(!ptr.empty())
-          {
-            BSTNode* temp = ptr.front();
-            ptr.pop();
-            
-            std::swap(temp->left, temp->right);
-            
-            if(temp->left) ptr.push(temp->left);
-            if(temp->right) ptr.push(temp->right);
-            
-          }
+    				ptr.push(root);
+			 	while(!ptr.empty())
+			  	{
+			   		BSTNode* temp = ptr.front();
+			    		ptr.pop();
 
+			    		std::swap(temp->left, temp->right);
+
+			    		if(temp->left) ptr.push(temp->left);
+			    		if(temp->right) ptr.push(temp->right);
+
+			  	}
 			}
 	};
 }
 
-/* Example Usage 
-
-int main(void)
-{
-  using namespace ads;
-  BinarySearchTree<int> tree(100); 
-
-  tree.insertValue(20);
-  tree.insertValue(50);
-  tree.insertValue(60);
-  std::cout << "In Order Traversal" << std::endl;
-  tree.traverseInOrder();
-
-  tree.insertValue(90);
-  tree.insertValue(200);
-  std::cout << "Pre Order Traversal" << std::endl;
-  tree.traversePreOrder();
-
-  tree.insertValues({99, 120, 300, 60, 34});
-  std::cout << "Post Order Traversal" << std::endl;
-  tree.traversePostOrder();
-
-
-  std::cout << "Find Key 61: " << tree.keySearch(61) << std::endl;
-  std::cout << "Height: " << tree.depth() << std::endl;
-
-   std::cout << "In Order Regular Tree: " << std::endl;
-  tree.traverseInOrder([](int a) { std::cout << "Custom Lambda Printed Key: " << a << std::endl; });
-  tree.invertTree();
-  std::cout << "In Order Inverted Tree: " << std::endl;
-  tree.traverseInOrder();
-
-}
-
-*/
 
 #endif
